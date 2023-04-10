3176e9e21f823d5db57b8b53b168138cb368254a
ecometto <ecometto@hotmail.comm>
2023-04-10 11:39:18 -0300

    probando cambio de nombre en github

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function index() {
     <Layout>
       <div className="container text-center">
         <p> On the top of this page, you will find a nav bar with links what will redirect you to a list of "users" and "posts". </p>
-        <p> The data into this lists where collected from "JsonPlaceHolder FAKE FREE API"</p>
+        <p> The data into this lists where collected from "JsonPlaceHolder FAKE FREE API".</p>
         <p>Users data was server side renderized, while Posts data was Client side renderized, in order to practice both ways to renderice information obtained (had got?) from APIs</p>
       </div>
     </Layout>
